d01987cec483886da509040aaf266e5d08d395c0
Ilya <wickedbun@yandex.ru>
2022-04-01 16:49:26 +0500

    Created Setter and simple store

--- src/store/reducers/currentThemeReducer.js
+++ src/store/reducers/currentThemeReducer.js
@@ -1,10 +1,10 @@
 import { defaultState } from "../index.js";
 import { ACTIONS } from './../actions/index';
 
-export const currentThemeReducer = (state=defaultState, action) => {
+export const currentThemeReducer = (state = defaultState, action) => {
   switch (action.type) {
     case ACTIONS.CHANGE_CURRENT_THEME:
-      return {...state, currentTheme: {mainColor: action.mainColor, secondColor: action.secondColor}}
+      return { ...state, currentTheme: { mainColor: action.mainColor, secondColor: action.secondColor } }
     default:
       return state
   }
