6892bce11534ff45fd6c631514904857aca58e34
Hukak_He_Pak <valsidalvonilb@outlook.com>
2022-04-18 16:08:07 +0400

    feat: add perfect board background (moveable), todo: thinking about mobile usability

--- src/components/Editor/Board.jsx
+++ src/components/Editor/Board.jsx
@@ -11,6 +11,7 @@ import { getCurrentBoard } from "../../store/selectors/board/getCurrentBoard";
 import { changeScale } from "../../store/actions/board/changeScale";
 import { getNote } from "../../store/selectors/note/getNote";
 import { clearEvent } from "../../store/actions/clearEvent";
+import { selectEvent } from "../../store/actions/selectEvent";
 
 export function Board() {
   const notes = useSelector(getNotes);
@@ -21,21 +22,26 @@ export function Board() {
 
   const noteEvent = useSelector((state) => state.selects.event);
 
-  const note = useSelector(getNote(noteEvent?.target?.noteId));
+  const note = useSelector(getNote(noteEvent?.noteId));
 
-  function moveHandler({ buttons, movementX, movementY }) {
-    if (buttons !== 1) return;
+  function moveHandler(event) {
+    const { buttons, movementX, movementY } = event;
+
+    
+
+    if (buttons !== 1 && event.type !== "onTouchMove") return;
 
     if (note) {
-      const { type } = noteEvent;
+      const { type, position } = noteEvent;
+      console.log(type);
 
       switch (type) {
         case "replace":
           dispatch(
             editNote(note.id, {
               position: {
-                top: note.position.top + movementY / board.scale,
-                left: note.position.left + movementX / board.scale,
+                top: note.position.top - (position.top - event.clientY) / board.scale,
+                left: note.position.left - (position.left - event.clientX) / board.scale,
               },
             })
           );
@@ -45,16 +51,20 @@ export function Board() {
           dispatch(
             editNote(note.id, {
               size: {
-                width: note.size.width + movementX / board.scale,
-                height: note.size.height + movementY / board.scale,
+                width: note.size.width - (position.left - event.clientX) / board.scale,
+                height: note.size.height - (position.top - event.clientY) / board.scale,
               },
             })
           );
+          
           break;
 
         default:
           break;
       }
+
+      
+      dispatch(selectEvent({ ...noteEvent, position: { left: event.clientX, top: event.clientY }}));
       return;
     }
 
@@ -96,10 +106,15 @@ export function Board() {
 
     dispatch(
       changeScrollPos(board.id, {
-        top: board.position.top + (clientY / scale - clientY / board.scale) //+ event.clientX / 10 * (event.deltaY % 2),
-        , left: board.position.left + (clientX / scale - clientX / board.scale)// + event.clientY / 10 * (event.deltaY % 2),
+        top: board.position.top + (clientY / scale - clientY / board.scale), //+ event.clientX / 10 * (event.deltaY % 2),
+        left: board.position.left + (clientX / scale - clientX / board.scale), // + event.clientY / 10 * (event.deltaY % 2),
       })
-    )
+    );
+  }
+
+  function mouseUpHandler(event) {
+    dispatch(clearEvent());
+    event.stopPropagation();
   }
 
   useEffect(() => {
@@ -112,23 +127,27 @@ export function Board() {
 
   return (
     <div
-      style={{
-        width: board.width,
-        height: board.height,
-        background: board.theme.color,
-      }}
       className="board"
-      onMouseMove={moveHandler}
       ref={boardNode}
-      onWheel={wheelHandler}
-      onMouseUp={(event) => {
-        dispatch(clearEvent());
-        event.stopPropagation();
+      style={{
+        // width: board.width,
+        // height: board.height,
+        background: `url(https://catherineasquithgallery.com/uploads/posts/2021-02/1613227411_48-p-fon-sinii-geometriya-62.jpg)`,
+        backgroundPosition: `${board.position.left * board.scale / 2}px ${board.position.top * board.scale / 2}px`,
+        backgroundSize: 1000 * board.scale
       }}
+      //onTouchStart={console.log}
+      onMouseMove={moveHandler}
+      //onTouchMove={moveHandler}
+      onWheel={wheelHandler}
+      onMouseUp={mouseUpHandler}
+      onTouchEnd={mouseUpHandler}
+      //onTouchCancel={console.log}
     >
       {notes?.map((note) => {
         const { id } = note;
         const selected = id === selectedNoteId;
+
         return (
           <NoteWrap key={id} selected={selected} note={note}>
             {CreateNote(note, selected)}
